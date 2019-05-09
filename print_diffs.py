ee618b74991b8e4b7d49e00215a3facd14047a6e
port <oleg8angel@gmail.com>
2019-05-09 09:09:00 +0300

    print_diffs script update

--- print_diffs.py
+++ print_diffs.py
@@ -7,6 +7,11 @@ client = MongoClient()
 db = client['hydralians']
 collection = db['products']
 
+diff_counter = 0
+
 for product in MongoRepo.read_all():
     if product.get('diff') and product.get('diff').get('old') != product.get('diff').get('new'):
         pprint(product['diff'])
+        diff_counter += 1
+
+print(diff_counter)
